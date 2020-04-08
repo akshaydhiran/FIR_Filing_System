f03cafaa96aea23318832a9a1d472591c9190c3b
akshaysingh5 <akshay2108akshay@gmail.com>
2020-04-08 10:54:08 +0530

    Takki

---
+++ app/src/main/java/MenuActivities/Fill_FIR_Activity.java
@@ -0,0 +1,16 @@
+package MenuActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fir_filing_system.R;
+
+public class Fill_FIR_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill__f_i_r_);
+    }
+}
---
+++ app/src/main/java/MenuActivities/ReportCrimeActivity.java
@@ -0,0 +1,16 @@
+package MenuActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fir_filing_system.R;
+
+public class ReportCrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_crime);
+    }
+}
